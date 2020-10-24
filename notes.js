bc476899c6e01ccc300f395c5922085264da8302
Darwin Vaz <dvaz@purdue.edu>
2020-10-24 17:19:36 -0400

    setup

---
+++ notes.js
@@ -0,0 +1,25 @@
+function createCookie(key, value) {
+    let cookie = escape(key) + "=" + escape(value) + ";";
+    document.cookie = cookie;
+    console.log(cookie);
+    console.log("Creating new cookie with key: " + key + " value: " + value);
+}
+createCookie("age", "21");
+
+
+function readCookie(name) {
+    let key = name + "=";
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(key) === 0) {
+            return cookie.substring(key.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+console.log(readCookie("icecream"));
