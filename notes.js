f26a7cfb7a3f1a3dcfeb63ed06fff0c6ada33d7f
Darwin Vaz <dvaz@purdue.edu>
2020-10-24 23:03:51 -0400

    added styles

--- notes.js
+++ notes.js
@@ -1,43 +1,68 @@
-let file = readCookie('file');
-if (file != null) {
-    JSON.parse(file);
+let tabs = localStorage.getItem('tabs');
+if (tabs == null) {
+    tabs = 1;
+    localStorage.setItem('tabs', '1');
+    localStorage.setItem('titles', 'Default');
+    localStorage.setItem('page0', 'Some notes here');
 }
 
-document.getElementById('inputfile').addEventListener('change', function() {
-    let fr = new FileReader();
-    let fileObject = this.files[0];
-    fr.readAsText(fileObject);
-    var newObject  = {
-        'lastModified'     : fileObject.lastModified,
-        'lastModifiedDate' : fileObject.lastModifiedDate,
-        'name'             : fileObject.name,
-        'size'             : fileObject.size,
-        'type'             : fileObject.type
-    };
-    console.log(fileObject);
-    console.log(newObject);
-    createCookie('file', JSON.stringify(newObject));
-});
+let titles = localStorage.getItem('titles').split(',');
+let banner = document.getElementById('tab');
 
-function createCookie(key, value) {
-    let date = new Date();
-    date.setDate(date.getDate() + 365);
-    let expiration = date.toUTCString();
-    let cookie = escape(key) + "=" + escape(value) + ";expires=" + expiration + ";";
-    document.cookie = cookie;
- }
+let pages = [];
+for (let i = 0; i < tabs; i++) {
+    pages[i] = localStorage.getItem('page' + i);
+    let button = document.createElement('button');
+    button.innerHTML = titles[i];
+    let click = document.createAttribute('onclick');
+    click.value = 'openpage(' + i + ')';
+    let edit = document.createAttribute('contenteditable');
+    edit.value = 'true';
+    button.setAttributeNode(click);
+    button.setAttributeNode(edit);
+    banner.appendChild(button);
+}
 
-function readCookie(name) {
-    let key = name + "=";
-    let cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i];
-        while (cookie.charAt(0) === ' ') {
-            cookie = cookie.substring(1, cookie.length);
-        }
-        if (cookie.indexOf(key) === 0) {
-            return cookie.substring(key.length, cookie.length);
+let button = document.createElement('button');
+button.innerHTML = '+';
+button.id = 'plus';
+let attr = document.createAttribute('onclick');
+attr.value = 'newpage()';
+button.setAttributeNode(attr);
+banner.appendChild(button);
+
+let pane = document.getElementById('pane');
+pane.innerHTML = pages[0];
+
+function openpage(num) {
+    pane.innerHTML = pages[num];
+    let butts = document.getElementsByTagName('button');
+
+    for (let i = 0 ; i < butts.length - 1; i++) {
+        if (num == i) {
+            butts[i].style = "background-color: #ccc;";
+        } else {
+            butts[i].style = "background-color: inherit;"
         }
     }
-    return null;
 }
+
+function newpage() {
+    localStorage.setItem('page' + tabs, '');
+
+    let button = document.createElement('button');
+    button.innerHTML = 'NewPage';
+    let click = document.createAttribute('onclick');
+    click.value = 'openpage(' + tabs + ')';
+    button.setAttributeNode(click);
+    banner.insertBefore(button, document.getElementById('plus'));
+
+    tabs++;
+    localStorage.setItem('tabs', tabs);
+    localStorage.setItem('titles', titles + ',New Page');
+}
+
+
+// document.getElementById("editor").addEventListener("input", function() {
+//     console.log("input event fired");
+// }, false);
